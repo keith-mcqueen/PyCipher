b3c637f138da9d8174dc3364b313d885a9787877
Keith McQueen <keith.mcqueen@macweenies.com>
2014-09-11 15:05:41 -0600

    Fixed the ff_multiply() and x_time() methods.

--- pycipher.py
+++ pycipher.py
@@ -1,5 +1,10 @@
 __author__ = 'keith'
 
+modulo = 0x11b
+high_order_bit = 0x100
+min_x_time_bit = 0x01
+default_x_time_bit = 0x02
+
 
 def ff_multiply(operand, times):
     # if input or times <= 0, then return 0
@@ -12,24 +17,37 @@ def ff_multiply(operand, times):
     if times == 1:
         return operand
 
-    product = x_time(operand) + ff_multiply(operand, times - 2)
-    # product = x_time(operand) + ff_multiply(x_time(operand), times - 2)
-    # product = x_time(operand)
-    # product += ff_multiply(product, times - 2)
+    # start with a 0 product that we'll accumulate
+    product = 0
 
-    return normalize(product)
+    # start at the lowest order bit
+    current_bit = 0x01
+
+    # loop until we've hit all the bits of 'times'
+    while current_bit <= times:
+        # if the current bit of 'times' is set, then do x_time for the current bit, adding the result to the product
+        if times & current_bit == current_bit:
+            product = ff_add(product, x_time(operand, current_bit))
+
+        # increment the bit (left shift one place)
+        current_bit <<= 1
+
+    return product
 
 
-def x_time(operand):
-    # multiply by 2 (left shift bits one place)
-    return normalize(operand << 1)
+def x_time(operand, bit=default_x_time_bit):
+    # if we've hit the minimum bit, then just return the operand
+    if bit <= min_x_time_bit:
+        return operand
+
+    # multiply the operand by 2 (left shift one place), while decrementing the bit (right shift one place)
+    return x_time(normalize(operand << 1), bit >> 1)
 
 
 def normalize(num):
-    # if the product is too big, then modulo it by 0x11b
-    if num > 0x11b:
-        #return normalize(num ^ 0x11b)
-        return num ^ 0x11b
+    # if the product is too big, then modulo (just XOR) it by 0x11b
+    if num & high_order_bit == high_order_bit:
+        return num ^ modulo
 
     return num
 
@@ -39,25 +57,27 @@ def ff_add(num1, num2):
     return num1 ^ num2
 
 # this should be 0x57 - good
-print '0x57 . 0x01 = ' + hex(ff_multiply(0x57, 0x01))
-print
+# print '0x57 . 0x01 = ' + hex(ff_multiply(0x57, 0x01))
+# print
 
 # this should be 0xae - good
-print '0x57 . 0x02 = ' + hex(ff_multiply(0x57, 0x02))
-print
+# print '0x57 . 0x02 = ' + hex(ff_multiply(0x57, 0x02))
+# print
 
 # this should be 0x47 - good
-print '0x57 . 0x04 = ' + hex(ff_multiply(0x57, 0x04))
-print
+# print '0x57 . 0x04 = ' + hex(ff_multiply(0x57, 0x04))
+# print
 
-# there is something wrong with my logic on how I'm doing this.  It may be that the problem is in my recursion, but I'm
-# really sure.
-# this should be 0x8e - wrong: I'm getting 0xb8
-print '0x57 . 0x08 = ' + hex(ff_multiply(0x57, 0x08))
-print
+# this should be 0x8e - good
+# print '0x57 . 0x08 = ' + hex(ff_multiply(0x57, 0x08))
+# print
 
-# this should be 0x07 - wrong: I'm getting 0x97
-print '0x57 . 0x10 = ' + hex(ff_multiply(0x57, 0x10))
-print
+# this should be 0x07 - good
+# print '0x57 . 0x10 = ' + hex(ff_multiply(0x57, 0x10))
+# print
 
-print(hex(x_time(0x8e)))
+# this should be 0x07 - good
+# print '0x57 . 0x13 = ' + hex(ff_multiply(0x57, 0x13))
+# print
+
+# print hex(x_time(x_time(x_time(x_time(0x57)))))
